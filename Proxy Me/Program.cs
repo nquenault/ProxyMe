ab91094db60c043bc652010e61e393d7774e8b79
nquenault <nquenault@gmail.com>
2017-06-29 22:28:49 +0200

    test

--- Proxy Me/Program.cs
+++ Proxy Me/Program.cs
@@ -16,7 +16,8 @@ namespace ProxyMe
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            var main = new Main();
+            Application.Run();
         }
     }
 }
